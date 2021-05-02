d6c33efb1503f8118af31d2e44320288675cf93a
Satoru Yokoyama <22640673+chao7150@users.noreply.github.com>
2021-05-03 01:57:36 +0900

    feat: minimum application

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,19 @@
-FROM node:latest AS builder
-
-WORKDIR /usr/src/app
-COPY . ./
-RUN yarn install
-RUN yarn build
-
-FROM node:latest
-
+FROM node:15.11.0-slim AS deps
 WORKDIR /usr/src/app
 COPY package.json yarn.lock ./
 RUN yarn install --production
+
+FROM node:15.11.0-slim AS builder
+WORKDIR /usr/src/app
+COPY . ./
+COPY --from=deps /usr/src/app/node_modules ./node_modules
+RUN yarn install
+RUN yarn build
+
+FROM node:15.11.0-alpine
+WORKDIR /usr/src/app
+COPY package.json yarn.lock ./
+COPY --from=deps /usr/src/app/node_modules ./node_modules
 COPY --from=builder /usr/src/app/build ./build/
 
 HEALTHCHECK CMD curl -f http://localhost:3000/status || exit 1
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-export const add = (x: number, y: number): number => x + y;
+import fastify from "fastify";
+import { Static, Type } from "@sinclair/typebox";
+
+const app = fastify({ logger: true });
+
+const Titles = Type.Object({
+  titles: Type.String(),
+});
+type TitlesType = Static<typeof Titles>;
+
+app.get<{ Querystring: TitlesType }>(
+  "/work-id",
+  {
+    schema: {
+      querystring: Titles,
+      response: {
+        200: {
+          type: "array",
+          items: { type: "integer" },
+        },
+      },
+    },
+  },
+  async (request, reply) => {
+    return request.query.titles;
+  },
+);
+
+app.get("/status", async (request, reply) => {
+  return "OK";
+});
+
+const start = async () => {
+  try {
+    // デフォルトの127.0.0.1だとdockerの外から通信を受けられない
+    await app.listen(3000, "0.0.0.0");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
